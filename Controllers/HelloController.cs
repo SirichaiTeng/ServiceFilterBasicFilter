29ec3123455d716af51229afe805dfabb1daf858
SirichaiTeng <chainis.teon@gmail.com>
2024-09-09 12:11:31 +0700

    Frist Commit

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ServiceFilterBasic.Models; // <-- using
+namespace ServiceFilterBasic.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("getHello")]
+        [ServiceFilter(typeof(ApiKeyAuthorizationFilter))]
+        public IActionResult GetHello()
+        {
+            string result = "Hello World";
+            return Ok(JsonConvert.SerializeObject(result, Newtonsoft.Json.Formatting.Indented));
+        }
+    }
+}
---
+++ Models/ApiKeyAuthorizationFilter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+
+namespace ServiceFilterBasic.Models
+{
+    public class ApiKeyAuthorizationFilter : IAuthorizationFilter
+    {
+        private const string APIKEY = "x-api-key";
+        private readonly string _apikey;
+
+        public ApiKeyAuthorizationFilter(IConfiguration configuration)
+        {
+            _apikey = configuration["ApiKey"].ToString();
+        }
+
+
+        // interface IAuthorizationFilter <-
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!context.HttpContext.Request.Headers.TryGetValue(APIKEY, out var extractedApiKey))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+            if (!_apikey.Equals(extractedApiKey))
+            {
+                context.Result = new UnauthorizedResult();
+            }
+        }
+
+    }
+}
+
